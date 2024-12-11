edba3649d5c5e26ead45b1cd02f3c5759402c8a9
rotemadimor <54247736+rotemadimor@users.noreply.github.com>
2024-12-11 23:54:24 +0200

    Add files

---
+++ index.js
@@ -0,0 +1,9 @@
+var subjectObject = {
+    "HTML": ["Links", "Images", "Tables", "Lists"],
+    "CSS": ["Borders", "Margins", "Backgrounds", "Float"],
+    "JavaScript": ["Variables", "Operators", "Functions", "Conditions"]    
+}
+var subjectSel = document.getElementById("subject");
+function pickSubject(){
+
+}
