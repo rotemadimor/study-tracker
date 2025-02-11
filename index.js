8f7a00d49a536065cc53fb12e7ee197b086255a7
rotemadimor <rotemadimor@gmail.com>
2025-02-11 23:47:37 +0200

    Updated subjetManager.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+import { SubjectManager } from './SubjectManager.js';
+
 var subjectObject = {
     "HTML": ["Links", "Images", "Tables", "Lists"],
     "CSS": ["Borders", "Margins", "Backgrounds", "Float"],
     "JavaScript": ["Variables", "Operators", "Functions", "Conditions"]    
 }
+
 var subjectSel = document.getElementById("subject");
 function pickSubject(){
 
 }
+
+function addNewSubject(newName){
+
+
+}
+
