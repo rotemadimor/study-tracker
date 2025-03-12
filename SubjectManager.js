0bb38f358c44e0cca6d18c919b658bb13e434782
rotemadimor <rotemadimor@gmail.com>
2025-03-12 13:28:03 +0200

    subject manager

--- SubjectManager.js
+++ SubjectManager.js
@@ -1,3 +1,15 @@
 export class SubjectManager{
-    
+    static subjects = new Set();
+
+    static addSubject(newSubject){
+        if(this.subjects.has(newSubject)){
+            console.log('Subject "${subjectName}" already exists.');
+        } else{
+            this.subjects.add(newSubject);
+        }
+    }
+
+    static getSubjects(){
+        return this.subjects;
+    }
 }
--- index.js
+++ index.js
@@ -1,18 +1,5 @@
 import { SubjectManager } from './SubjectManager.js';
 
-var subjectObject = {
-    "HTML": ["Links", "Images", "Tables", "Lists"],
-    "CSS": ["Borders", "Margins", "Backgrounds", "Float"],
-    "JavaScript": ["Variables", "Operators", "Functions", "Conditions"]    
-}
-
 var subjectSel = document.getElementById("subject");
-function pickSubject(){
 
-}
-
-function addNewSubject(newName){
-
-
-}
 
