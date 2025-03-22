1b922b1c47c28f2776befb0c9ab5fbce492f32bc
rotemadimor <54247736+rotemadimor@users.noreply.github.com>
2025-03-22 10:07:29 +0200

    February version

--- SubjectManager.js
+++ SubjectManager.js
@@ -1,15 +1,20 @@
+import { SubjectTimer } from './SubjectTimer.js';
+
 export class SubjectManager{
-    static subjects = new Set();
+    static subjects = new Map();
+    constructor() {
+        this.subjects = new Map();
+    }
 
     static addSubject(newSubject){
         if(this.subjects.has(newSubject)){
             console.log('Subject "${subjectName}" already exists.');
-        } else{
-            this.subjects.add(newSubject);
+        }
+         else{
+            var newSubjectTimer = new SubjectTimer(newSubject);
+            this.subjects.set(newSubject,newSubjectTimer);
         }
     }
 
-    static getSubjects(){
-        return this.subjects;
-    }
+    static getSubjects(){return this.subjects;}
 }
--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import { SubjectManager } from './SubjectManager.js';
 
-var subjectSel = document.getElementById("subject");
+var subjectSelected = document.getElementById("subjectChosen");
+var submitSubject = document.getElementById("submitSubject");
 
 
+document.addEventListener("DOMContentLoaded", () => {
+    submitSubject.addEventListener("click", function() {
+        SubjectManager.addSubject(subjectSelected.textContent);
+        console.log(SubjectManager.getSubjects());
+    });
+});
